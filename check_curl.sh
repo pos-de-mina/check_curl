872da5d4b585635c7c4e5836f5dfa30882872ad2
António Pós-de-Mina <pos-de-mina@users.noreply.github.com>
2020-09-15 23:16:21 +0100

    Update check_curl.sh

--- check_curl.sh
+++ check_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#
+# /omd/_custom/plugins/check_curl.sh
 # R E F
-#     - https://curl.haxx.se/docs/manpage.html
+#   - https://curl.haxx.se/docs/manpage.html
 #
 # (c) 2019-06-22 Antonio Pos-de-Mina
 
@@ -9,31 +9,33 @@ curl_url=$1
 curl_proxy=$2
 curl_metrics='curl_http_code=%{http_code}\ncurl_time_total=%{time_total}\ncurl_size_download=%{size_download}\n'
 
-# clean last call
-rm -f ~/tmp/curl_log_*
+export status=0
 
-curl $curl_url --proxy $curl_proxy -w $curl_metrics --output ~/tmp/curl_log_stdout_$$ --stderr ~/tmp/curl_log_stderr_$$ --trace ~/tmp/curl_log_trace-$$ > ~/tmp/curl_log_metrics-$$
+curl $curl_url --proxy $curl_proxy -w $curl_metrics --output ~/tmp/curl_log_$$.stdout --stderr ~/tmp/curl_log_$$.stderr --trace ~/tmp/curl_log_$$.trace > ~/tmp/curl_log_$$.metrics
 curl_rc=$?
 
-if [ "$curl_rc" -eq "0" ]
+if (( $curl_rc = 0 ))
 then
-        source ~/tmp/curl_log_metrics-$$
-        echo -e "HTTP $curl_http_code; Response Time $curl_time_total; Response Size $curl_size_download\n$(cat ~/tmp/curl_log_stdout_$$)|Response_Time=$curl_time_total Response_Size=$curl_size_download HTTP_Return_Code=$curl_http_code"
-        if [ "$curl_http_code" -ge "500" ]
-        then
-                exit 2
-        elif [ "$curl_http_code" -ge "400" ]
-        then
-                exit 1
-        elif [ "$curl_http_code" -ge "300" ]
-        then
-                exit 1
-        else
-                exit 0
-        fi
+  source ~/tmp/curl_log_$$.metrics
+  echo -e "HTTP $curl_http_code; Response Time $curl_time_total; Response Size $curl_size_download\n$(cat ~/tmp/curl_log_stdout_$$)|Response_Time=$curl_time_total Response_Size=$curl_size_download HTTP_Return_Code=$curl_http_code"
+  if [ "$curl_http_code" -ge "500" ]
+  then
+    export status=2
+  elif [ "$curl_http_code" -ge "400" ]
+  then
+    export status=1
+  elif [ "$curl_http_code" -ge "300" ]
+  then
+    export status=1
+  fi
 else
-        # error code
-        curl_error=$(awk -v line=$curl_rc 'NR == line{print $0}' /omd/scripts/curl_error_map.txt)
-        echo "$curl_error|Response_Time=$curl_time_total Response_Size=$curl_size_download HTTP_Return_Code=$curl_http_code"
-        exit 2
+  # error code
+  curl_error=$(awk -v line=$curl_rc 'NR == line{print $0}' /omd/_custom/plugins/curl_error_map.txt)
+  echo "$curl_error|Response_Time=$curl_time_total Response_Size=$curl_size_download HTTP_Return_Code=$curl_http_code"
+  export status=2
 fi
+
+# clean last call
+rm -f ~/tmp/curl_log_$$*
+
+exit $status
